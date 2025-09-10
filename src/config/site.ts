6f84b162f7de5c319d64af758a1429e37cb9c0ac
lane Warr <lanewarrr@gmail.com>
2025-09-10 06:15:52 +0000

    effacer dobc les images que ji faites avec les url

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,5 +3,25 @@
 // By putting the hero image URL here, it's easy to change it in the future
 // without having to hunt through component files.
 export const siteConfig = {
-  heroImage: "https://i.postimg.cc/ZKwnrFQb/smiley-femme-parlant-au-telephone-coup-moyen-1.png",
+  heroImage: "/hero-background.png",
+  services: [
+      {
+        image: "/service-personal-loan.jpg",
+      },
+      {
+        image: "/service-mortgage-loan.jpg",
+      },
+      {
+        image: "/service-car-loan.jpg",
+      },
+      {
+        image: "/service-business-loan.jpg",
+      },
+      {
+        image: "/service-debt-consolidation.jpg",
+      },
+      {
+        image: "/service-student-loan.jpg",
+      }
+  ]
 };
