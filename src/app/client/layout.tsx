89b3f7a08d8dd11e058ae9d79ed719281623dfb1
lane Warr <lanewarrr@gmail.com>
2025-09-12 16:42:19 +0000

    pas encoreb resolu

--- src/app/client/layout.tsx
+++ src/app/client/layout.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-secondary/20 dark:bg-background">
-        <div className="container py-8 md:py-12">
+        <div className="container mx-auto py-8 md:py-12">
            {children}
         </div>
     </div>
