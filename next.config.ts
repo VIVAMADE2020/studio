be50e4d88b54cb3b2f128aa9ac28c4af08b735c3
lane Warr <lanewarrr@gmail.com>
2025-09-16 17:38:52 +0000

    si non ca ne saffiche pas cest le logo de firebase qui saffiche

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,15 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/favicon.ico',
+        destination: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><circle cx=%2250%22 cy=%2250%22 r=%2250%22 fill=%22hsl(215, 39%, 29%)%22 /><text y=%22.9em%22 x=%2250%%22 text-anchor=%22middle%22 font-size=%2260%22 font-family=%22Arial%22 fill=%22hsl(210, 20%, 98%)%22>F</text></svg>',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
